1cecd10988cbc81ae237bee0fe9d37b4bcf7cbf3
jaimovich andres <andijaimo@gmail.com>
2024-01-15 00:24:34 +0000

    tests with static files

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'saranda': 'saras',
+        'gaspacho': 31
+    }
+    return render(request, 'base/index.html', context=context)
